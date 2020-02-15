61e44ce80470d4a167cede4bbe93ff717fbd97a1
Ben Awad <benawad97@gmail.com>
2020-02-14 18:30:52 -0600

    add spec-files graph test

--- tests/build-graph.test.ts
+++ tests/build-graph.test.ts
@@ -3,7 +3,9 @@ import path from "path";
 
 const t = (folder: string, g: any) => {
   it(folder, () => {
-    expect(buildGraph(path.join(__dirname, "fixtures", folder)).graph).toEqual(g);
+    expect(buildGraph(path.join(__dirname, "fixtures", folder)).graph).toEqual(
+      g
+    );
   });
 };
 
@@ -18,5 +20,13 @@ describe("build graph", () => {
     "routes/index.js": ["home/index.js"],
     "login/index.js": ["utils/search.js"],
     "utils/search.js": ["index.js"]
+  });
+
+  t("spec-files", {
+    "index.js": ["level1.js"],
+    "index.spec.js": ["index.js"],
+    "level1.spec.js": ["level1.js"],
+    "level1.js": ["level2.js"],
+    "level2.spec.js": ["level2.js"]
   });
 });
