7e409191113a07e85c98547cc48e19e51f74366a
Wayne Van Son <waynevspersonal@gmail.com>
2020-02-21 07:45:31 +1100

    adds description to "addEdge"

--- src/index/formatFileStructure/buildGraph/addEdge.ts
+++ src/index/formatFileStructure/buildGraph/addEdge.ts
@@ -1,5 +1,6 @@
 import { Graph } from "../shared/Graph";
 
+/** Adds an edge to the graph, adding the start if it doesn't already exist. */
 export function addEdge([start, end]: [string, string], graph: Graph) {
   if (!(start in graph)) {
     graph[start] = [];
