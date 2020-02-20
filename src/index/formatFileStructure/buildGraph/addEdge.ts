7decdf22dd191a79b416957f655d4cc904e905ad
Wayne Van Son <waynevspersonal@gmail.com>
2020-02-21 08:16:04 +1100

    renames "addEdge" to "addEdgeToGraph"

--- src/index/formatFileStructure/buildGraph/addEdge.ts
+++ src/index/formatFileStructure/buildGraph/addEdge.ts
@@ -1,7 +1,7 @@
 import { Graph } from "../shared/Graph";
 
 /** Adds an edge to the graph, adding the start if it doesn't already exist. */
-export function addEdge([start, end]: [string, string], graph: Graph) {
+export function addEdgeToGraph([start, end]: [string, string], graph: Graph) {
   if (!(start in graph)) {
     graph[start] = [];
   }
