1b5bca10b3d29853feeb70f67e096f9b4a74ec3e
Oskar Grunning <oskargrunning@gmail.com>
2020-02-21 22:37:51 +0100

    fix(printTree): position our leafs

--- src/index/treePrinter.ts
+++ src/index/treePrinter.ts
@@ -5,12 +5,17 @@
 
 const createBranchFromParts = (parts: string[], count: number) =>
   parts.slice(0, count).join("/");
+
+const removePathDuplication = (target: string, match: string) =>
+  target.replace(new RegExp(`^(/*)${match.replace(/\//, "")}(/+)`), "$1$2");
+
 const sanitizeDots = (text: string) => text.replace(/\./g, "");
 const compareLeafs = (a: string, b: string) =>
   sanitizeDots(a).localeCompare(sanitizeDots(b));
 
-const resolveLeafs = (targets: string[]) => {
-  const leafs = targets.reduce<Set<string>>((acc, target) => {
+/** Resolve every unique leaf from a list of paths. */
+const resolveLeafs = (paths: string[]) => {
+  const leafs = paths.reduce<Set<string>>((acc, target) => {
     const parts = target.split("/");
 
     parts.forEach((_, idx) => acc.add(createBranchFromParts(parts, idx + 1)));
@@ -20,11 +25,36 @@ const resolveLeafs = (targets: string[]) => {
   return Array.from(leafs).sort(compareLeafs);
 };
 
-export const treePrinter = (tree: Record<string, string>) => {
-  const targets = Object.values(tree);
-  const leafs = resolveLeafs(targets);
+/**
+ * Iterates over all leafs and positions them on the correct branches, ie
+ * resolving their end name in relation to their branch and their position.
+ */
+const positionLeafs = (leafs: string[]) => {
+  const res = [];
+
+  let queue = [...leafs];
+  while (queue.length > 0) {
+    const leaf = queue.shift();
+
+    if (leaf == null) break;
+    queue = queue.map(queuedLeaf => removePathDuplication(queuedLeaf, leaf));
+
+    res.push(leaf);
+  }
+
+  return res.map(x => {
+    const parts = x.split("/");
+    return { text: parts.pop() ?? "", position: parts.length };
+  });
+};
+
+/** Prints a tree visualization of given paths. */
+export const treePrinter = (paths: string[]) => {
+  const leafs = resolveLeafs(paths);
+  const positionedLeafs = positionLeafs(leafs);
 
   console.log(leafs);
+  console.log(positionedLeafs);
 };
 
 export default treePrinter;
--- tests/treePrinter.test.ts
+++ tests/treePrinter.test.ts
@@ -1,15 +1,16 @@
 import treePrinter from "../src/index/treePrinter";
 
-treePrinter({
-  "index.js": "index.js",
-  "index.css": "index/index.css",
-  "App.js": "index/App.js",
-  "foo.svg": "index/Foo/foo.svg",
-  "logo.svg": "index/App/logo.svg",
-  "App.css": "index/App/App.css",
-  "serviceWorker.js": "index/serviceWorker.js",
-  "App.test.js": "index/App.test.js",
-});
+treePrinter([
+  "index.js",
+  "index/index.css",
+  "index/App.js",
+  "index/Foo/foo.svg",
+  "index/Foo/Bar/bar.css",
+  "index/App/logo.svg",
+  "index/App/App.css",
+  "index/serviceWorker.js",
+  "index/App.test.js",
+]);
 
 describe("treePrinter", () => {
   it("should", () => {
