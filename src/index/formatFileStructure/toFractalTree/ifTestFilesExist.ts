21d5ebde86124dded8f67bd1d9718c750ff9bbac
Wayne Van Son <waynevspersonal@gmail.com>
2020-02-23 09:46:21 +1100

    creates "ifTestFilesExist" from "toFractalTree"

---
+++ src/index/formatFileStructure/toFractalTree/ifTestFilesExist.ts
@@ -0,0 +1,43 @@
+import { FractalTree } from "../toFractalTree";
+
+import path from "path";
+
+import { checkDuplicatesInTree } from "./checkDuplicatesInTree";
+import { Graph } from "../shared/Graph";
+
+export function ifTestFilesExist(
+  testFiles: Set<string>,
+  graph: Graph,
+  tree: FractalTree
+) {
+  // not being used currently.
+  // but returning in function for later.
+  const globalTests = [];
+
+  for (const testFile of testFiles) {
+    const [firstRelativeImport] = graph[testFile];
+    if (!firstRelativeImport) {
+      globalTests.push(testFile);
+      continue;
+    }
+
+    const testFilePath = tree[firstRelativeImport];
+    if (!testFilePath) continue;
+
+    const location = path.join(
+      path.dirname(testFilePath),
+      path.basename(testFile)
+    );
+
+    const newLocation = checkDuplicatesInTree({
+      location,
+      dirname: path.dirname(testFilePath),
+      filePath: testFile,
+      tree,
+    });
+
+    tree[testFile] = newLocation;
+  }
+
+  return { globalTests };
+}
