b31eba3b3f2dbf6e8d0edd4d57ede1cb8ab32dca
Wayne Van Son <waynevspersonal@gmail.com>
2020-02-23 09:48:36 +1100

    seperates recursive function from body of "toFractalTree"

--- src/index/formatFileStructure/toFractalTree.ts
+++ src/index/formatFileStructure/toFractalTree.ts
@@ -1,137 +1,30 @@
-import path from "path";
-
-import logger from "../../shared/logger";
 import { Graph } from "./shared/Graph";
-import { findSharedParent } from "./shared/findSharedParent";
-import { hasCycle } from "./toFractalTree/hasCycle";
-import { isTestFile } from "./shared/isTestFile";
+import { createFractalProperties } from "./toFractalTree/createFractalProperties";
+import { ifTestFilesExist } from "./toFractalTree/ifTestFilesExist";
+import { notACycle } from "./toFractalTree/notACycle";
+
+/** Property is the name, value is the name. */
+export type FractalTree = Record<string, string>;
+
+/** A list of dependencies for each fileName. */
+export type DependenciesTree = Record<string, string[]>;
 
 export function toFractalTree(graph: Graph, entryPoints: string[]) {
-  const res: Record<string, string> = {};
-  const dependencies: Record<string, string[]> = {};
-  const testFiles = new Set<string>();
-  let containsCycle = false;
+  // tree is what is returned
 
-  const addDependency = (key: string, location: string) => {
-    if (!Array.isArray(dependencies[key])) {
-      dependencies[key] = [];
-    }
-    dependencies[key].push(location);
-  };
+  const {
+    containsCycle,
+    dependencies,
+    testFiles,
+    tree,
+  } = createFractalProperties(graph, entryPoints);
 
-  const checkDuplicates = (
-    location: string,
-    dirname: string,
-    filePath: string
-  ) => {
-    let newLocation;
+  // if no cycle, then create shared folders.
+  // add it to tree
+  if (!containsCycle) notACycle(dependencies, tree);
 
-    if (Object.values(res).includes(location)) {
-      newLocation = path.join(dirname, filePath.replace(/\//g, "-"));
-      logger.info(`File renamed: ${filePath} -> ${newLocation}`);
-    }
+  // if test files exist...
+  if (testFiles.size > 0) ifTestFilesExist(testFiles, graph, tree);
 
-    return newLocation ?? location;
-  };
-
-  const fn = (filePath: string, folderPath: string, graph: Graph) => {
-    const basenameWithExt = path.basename(filePath);
-    if (isTestFile(basenameWithExt)) {
-      testFiles.add(filePath);
-      return;
-    }
-    let folderName = path.basename(filePath, path.extname(filePath));
-    const upperFolder = path.basename(path.dirname(filePath));
-    const isGlobal = filePath.includes("..");
-    let location = isGlobal
-      ? filePath
-      : path.join(
-          folderPath,
-          folderName === "index" && upperFolder && upperFolder !== "."
-            ? upperFolder + path.extname(filePath)
-            : basenameWithExt
-        );
-
-    location = checkDuplicates(location, folderPath, filePath);
-    folderName = path.basename(location, path.extname(location));
-
-    if (!isGlobal) {
-      res[filePath] = location;
-    }
-    const imports = graph[filePath];
-
-    if (imports?.length > 0) {
-      const newDestination = path.join(folderPath, folderName);
-
-      for (const importFilePath of imports) {
-        if (importFilePath in res) {
-          const cycle = hasCycle(importFilePath, graph, new Set());
-
-          if (cycle) {
-            containsCycle = true;
-            logger.warn(`Dependency cycle detected: ${cycle.join(" -> ")}`);
-          } else {
-            addDependency(importFilePath, location);
-          }
-          continue;
-        }
-
-        addDependency(importFilePath, location);
-        fn(importFilePath, newDestination, graph);
-      }
-    }
-  };
-
-  for (const filePath of entryPoints) {
-    fn(filePath, "", graph);
-  }
-
-  if (!containsCycle) {
-    Object.entries(dependencies).forEach(([k, v]) => {
-      if (v.length > 1 && !k.includes("..")) {
-        const parent = findSharedParent(v);
-        const filename = path.basename(k);
-        const upperFolder = path.basename(path.dirname(k));
-
-        res[k] = path.join(
-          parent,
-          "shared",
-          path.basename(filename, path.extname(filename)) === "index" &&
-            upperFolder &&
-            upperFolder !== "."
-            ? upperFolder + path.extname(filename)
-            : filename
-        );
-      }
-    });
-  }
-
-  if (testFiles.size > 0) {
-    const globalTests = [];
-
-    for (const testFile of testFiles) {
-      const [firstRelativeImport] = graph[testFile];
-      if (!firstRelativeImport) {
-        globalTests.push(testFile);
-        continue;
-      }
-
-      const testFilePath = res[firstRelativeImport];
-      if (testFilePath) {
-        let location = path.join(
-          path.dirname(testFilePath),
-          path.basename(testFile)
-        );
-
-        location = checkDuplicates(
-          location,
-          path.dirname(testFilePath),
-          testFile
-        );
-        res[testFile] = location;
-      }
-    }
-  }
-
-  return res;
+  return tree;
 }
---
+++ src/index/formatFileStructure/toFractalTree/createFractalProperties.ts
@@ -0,0 +1,94 @@
+import path from "path";
+import logger from "../../../shared/logger";
+import { Graph } from "../shared/Graph";
+import { isTestFile } from "../shared/isTestFile";
+import { DependenciesTree, FractalTree } from "../toFractalTree";
+import { getPathProperties } from "./getFolderName";
+import { hasCycle } from "./hasCycle";
+import { createAddDependency } from "./createAddDependency";
+
+/** An entry point to recursively create properties. */
+export function createFractalProperties(graph: Graph, entryPoints: string[]) {
+  const mutable: MutableParams = {
+    containsCycle: false,
+    dependencies: {},
+    testFiles: new Set<string>(),
+    tree: {},
+  };
+
+  for (const filePath of entryPoints) {
+    recursiveFractalProperties({ filePath, graph }, mutable);
+  }
+  return mutable;
+}
+
+interface RecursiveParams {
+  filePath: string;
+  folderPath?: string;
+  graph: Graph;
+}
+
+interface MutableParams {
+  containsCycle: boolean;
+  dependencies: DependenciesTree;
+  testFiles: Set<string>;
+  tree: FractalTree;
+}
+
+/** Recursively constructs the tree and additional properties. */
+function recursiveFractalProperties(
+  { filePath, folderPath = "", graph }: RecursiveParams,
+  mutable: MutableParams
+) {
+  const basenameWithExt = path.basename(filePath);
+
+  if (isTestFile(basenameWithExt)) {
+    mutable.testFiles.add(filePath);
+    return;
+  }
+
+  const { folderName, filePathIsGlobal, location } = getPathProperties({
+    basenameWithExt,
+    filePath,
+    folderPath,
+    tree: mutable.tree,
+  });
+
+  if (!filePathIsGlobal) mutable.tree[filePath] = location;
+
+  const { hasImports, imports } = getImportsFromGraph(graph, filePath);
+  if (!hasImports) return mutable;
+
+  const newDestination = path.join(folderPath, folderName);
+
+  const addDependency = createAddDependency(mutable.dependencies);
+
+  // if import exist, do loop.
+  for (const importFilePath of imports) {
+    const importInTree = importFilePath in mutable.tree;
+    if (importInTree) {
+      const cycle = hasCycle(importFilePath, graph);
+      if (cycle !== null) {
+        mutable.containsCycle = true;
+        logDependencyCycle(cycle);
+        continue;
+      }
+    }
+    addDependency(importFilePath, location);
+    recursiveFractalProperties(
+      { filePath: importFilePath, folderPath: newDestination, graph },
+      mutable
+    );
+  }
+  return mutable;
+}
+
+function logDependencyCycle(cycle: string[]) {
+  logger.warn(`Dependency cycle detected: ${cycle.join(" -> ")}`);
+}
+
+function getImportsFromGraph(graph: Graph, filePath: string) {
+  const imports = graph[filePath] || [];
+  const hasImports = imports.length > 0;
+  return { imports, hasImports };
+}
