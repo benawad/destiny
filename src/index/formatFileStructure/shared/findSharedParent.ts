2142dcbc9128923c4f361b8e69265748bbc9e0cd
Wayne Van Son <waynevspersonal@gmail.com>
2020-02-23 20:54:00 +1100

    use "if" body instead of a break.

--- src/index/formatFileStructure/shared/findSharedParent.ts
+++ src/index/formatFileStructure/shared/findSharedParent.ts
@@ -10,8 +10,9 @@ export const findSharedParent = (paths: string[]) => {
   for (let i = 0; i < fragments[0].length; i++) {
     const fragment = fragments[0][i];
     const allFragmentsMatch = fragments.every(f => f[i] === fragment);
-    if (!allFragmentsMatch) break;
-    parentPaths.push(fragment);
+    if (allFragmentsMatch) {
+      parentPaths.push(fragment);
+    }
   }
   return parentPaths.join("/");
 };
