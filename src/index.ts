420f68d49606ee629c9c90571d1e5c77a62e370f
Wayne Van Son <waynevspersonal@gmail.com>
2020-02-23 13:53:03 +1100

    add CLI related functions back into index.ts file

--- src/index.ts
+++ src/index.ts
@@ -18,50 +18,6 @@ const defaultOptions: Options = {
   version: false,
 };
 
-const printVersion = () => console.log("v" + version);
-const printHelp = (exitCode: number) => {
-  console.log(
-    chalk`{blue destiny} - Prettier for file structures.
-
-{bold USAGE}
-
-  {blue destiny} [option...] [{underline path}]
-
-  The {underline path} argument can consist of either a {bold file path} or a {bold glob}.
-
-{bold OPTIONS}
-
-  -V, --version            output version number
-  -h, --help               output usage information
-  `
-  );
-
-  return process.exit(exitCode);
-};
-
-const parseArgs = (
-  args: any[]
-): { options: Partial<Options>; paths: string[] } =>
-  args.reduce(
-    (acc, arg) => {
-      switch (arg) {
-        case "-h":
-        case "--help":
-          acc.options.help = true;
-          break;
-        case "-V":
-        case "--version":
-          acc.options.version = true;
-          break;
-        default:
-          acc.paths.push(arg);
-      }
-
-      return acc;
-    },
-    { options: {}, paths: [] }
-  );
-
 export const run = async (args: string[]) => {
   const config: Partial<Options> =
     cosmiconfigSync("destiny").search()?.config ?? {};
@@ -88,8 +44,73 @@ export const run = async (args: string[]) => {
   }
 
   await formatFileStructure(filesToRestructure, filesToEdit);
-}
+};
 
 if (process.env.NODE_ENV !== "test") {
   run(argv.slice(2, argv.length));
+}
+
+type ParsedArgs = {
+  options: {
+    help: boolean;
+    version: boolean;
+    detectRoots: boolean;
+  };
+  paths: string[];
+};
+
+const defaultArgs: ParsedArgs = {
+  options: {
+    help: false,
+    version: false,
+    detectRoots: false,
+  },
+  paths: [],
+};
+
+function parseArgs(args: any[]): ParsedArgs {
+  return args.reduce((acc, arg) => {
+    switch (arg) {
+      case "-h":
+      case "--help":
+        acc.options.help = true;
+        break;
+      case "-V":
+      case "--version":
+        acc.options.version = true;
+        break;
+      case "-dr":
+      case "--detect-roots":
+        acc.options.detectRoots = true;
+        break;
+      default:
+        acc.paths.push(arg);
+    }
+    return acc;
+  }, defaultArgs);
+}
+
+function printVersion() {
+  return console.log("v" + version);
+}
+
+function printHelp(exitCode = 0) {
+  console.log(
+    chalk`{blue destiny} - Prettier for file structures.
+
+{bold USAGE}
+
+  {blue destiny} [option...] [{underline path}]
+
+  The {underline path} argument can consist of either a {bold file path} or a {bold glob}.
+
+{bold OPTIONS}
+
+  -V, --version            output version number
+  -h, --help               output usage information
+  -dr, --detect-roots      structure after the first level
+  `
+  );
+
+  return process.exit(exitCode);
 }
