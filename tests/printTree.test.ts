0d95cf0ef77b8ee4dab9d35204b787258371d1d5
Oskar Grunning <oskargrunning@gmail.com>
2020-02-24 18:43:35 +0100

    test(printTree): add basic printTree tests

--- tests/printTree.test.ts
+++ tests/printTree.test.ts
@@ -1,36 +1,26 @@
 import printTree from "../src/index/printTree";
 
-printTree([
-  "index.ts",
-  "index/getFilePaths.ts",
-  "shared/logger.ts",
-  "index/formatFileStructure.ts",
-  "index/formatFileStructure/buildGraph.ts",
-  "index/formatFileStructure/shared/findEdges.ts",
-  "index/formatFileStructure/buildGraph/addEdge.ts",
-  "index/formatFileStructure/shared/Graph.ts",
-  "index/formatFileStructure/shared/findSharedParent.ts",
-  "index/formatFileStructure/shared/customResolve.ts",
-  "index/formatFileStructure/findEntryPoints.ts",
-  "index/formatFileStructure/shared/isTestFile.ts",
-  "index/formatFileStructure/moveFiles.ts",
-  "index/formatFileStructure/toFractalTree.ts",
-  "index/formatFileStructure/toFractalTree/hasCycle.ts",
-  "index/formatFileStructure/removeEmptyFolders.ts",
-  "index/formatFileStructure/fixImports.ts",
-  "index/formatFileStructure/fixImports/makeImportPath.ts",
-  "index/shared/RootOption.ts",
-  "index/shared/Foo.ts",
-  "index/RootOption.ts",
-  "index/Foo.ts",
-  "foo/asd/asd.ts",
-  "foo/bar/baz/baz.ts",
-  "foo/bar/baz/qux.ts",
-  "foo/quop/baz/qux.ts",
-]);
+const filePathsList = [
+  ["foo", "bar", "baz", "qux"],
+  ["foo", "foo/bar", "foo/bar/baz", "foo/bar/baz/qux"],
+  ["foo", "foo/bar", "foo/baz", "foo/qux"],
+  [
+    "foo",
+    "foo/bar/a",
+    "foo/bar/b",
+    "foo/baz/a",
+    "foo/baz/b",
+    "foo/qux/a",
+    "foo/qux/b",
+  ],
+  ["foo", "foo/bar", "foo/bar/baz", "foo/bar/baz/qux", "qux"],
+  ["foo.ts", "bar", "bar/foo", "bar/foo/foo.ts", "bar/bar.ts"],
+];
 
 describe("printTree", () => {
-  it("should", () => {
-    expect(true).toBe(true);
+  filePathsList.forEach(filePaths => {
+    it("should visualize tree", () => {
+      expect("\n" + printTree(filePaths)).toMatchSnapshot();
+    });
   });
 });
