1c480bc69adc30436ce77daee6f029f94b336dc9
Oskar Grunning <oskargrunning@gmail.com>
2020-02-24 18:43:08 +0100

    test: repair end-to-end tests

--- tests/end-to-end.test.ts
+++ tests/end-to-end.test.ts
@@ -1,9 +1,9 @@
 import fs from "fs-extra";
-import path from "path";
-import { formatFileStructure } from "../src/index/formatFileStructure";
-import { buildGraph } from "../src/index/formatFileStructure/buildGraph";
-import treeDir from "tree-node-cli";
 import glob from "glob";
+import path from "path";
+import treeDir from "tree-node-cli";
+
+import { buildGraph } from "../src/index/formatFileStructure/buildGraph";
 import { run } from "../src/index";
 
 const tmpPath = path.join(__dirname, "tmp");
@@ -19,6 +19,7 @@ afterAll(() => {
 const treeDirWithContents = (dir: string) => {
   const files = fs.readdirSync(dir);
   let tree: Record<string, string> = {};
+
   for (const file of files) {
     const filePath = path.join(dir, file);
     if (fs.lstatSync(filePath).isDirectory()) {
@@ -39,16 +40,20 @@ const treeDirWithContents = (dir: string) => {
 describe("end-to-end", () => {
   const fixturePath = path.join(__dirname, "fixtures");
   const testCases = fs.readdirSync(fixturePath);
+
   for (const testCase of testCases) {
     const templateFolder = path.join(fixturePath, testCase);
     const copyPath = path.join(tmpPath, testCase);
+
     fs.copySync(templateFolder, copyPath);
     it(testCase, async () => {
       const rootPath =
         testCase === "globals" ? path.join(testCase, "src") : testCase;
-      await run([path.join(tmpPath, rootPath)]);
+
+      await run(["--write", path.join(tmpPath, rootPath)]);
       buildGraph(glob.sync(path.join(copyPath, "/**/*.*")));
       expect(treeDir(path.join(tmpPath, rootPath))).toMatchSnapshot();
+
       const treeContents = treeDirWithContents(copyPath);
       Object.keys(treeContents).forEach(k => {
         expect(treeContents[k]).toMatchSnapshot(k);
