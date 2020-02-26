ce9915eac72870135e30b9b895a4ad265a004266
Simon Jang <simon.jang.contact@gmail.com>
2020-02-26 21:29:07 +0100

    fix: Fix time complexity when searching for common parent dir

--- src/index/generateTrees/shared/findSharedParent.ts
+++ src/index/generateTrees/shared/findSharedParent.ts
@@ -2,16 +2,19 @@ import path from "path";
 
 /** Find the common parent directory between all paths. */
 export const findSharedParent = (paths: string[]) => {
-  if (paths.length === 1) return path.dirname(paths[0]);
+  const [firstPath] = paths;
+  if (paths.length === 1) return path.dirname(firstPath);
+
+  const [shortest, secondShortest] = paths.sort((a, b) => a.length - b.length);
 
   const parentPaths: string[] = [];
-  const parts: string[][] = paths.map(x => x.split("/"));
-  const firstParts = parts[0];
+  const [shortestParts, secondShortestParts] = [
+    shortest,
+    secondShortest,
+  ].map(x => x.split("/"));
 
-  firstParts.forEach((part, idx) => {
-    const allPartsMatch = parts.every(p => p[idx] === part);
-
-    if (allPartsMatch) {
+  shortestParts.forEach((part, idx) => {
+    if (part === secondShortestParts[idx]) {
       parentPaths.push(part);
     }
   });
---
+++ tests/find-shared-parent.test.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import glob from "glob";
+import fs from "fs";
+
+import { findSharedParent } from "../src/index/generateTrees/shared/findSharedParent";
+
+const t = (folder: string, g: string) => {
+  it(folder, () => {
+    expect(
+      findSharedParent(
+        glob.sync(path.join(__dirname, "fixtures", folder, "/**/*.js"))
+      )
+    ).toEqual(g);
+  });
+};
+
+describe("findSharedParentTest", () => {
+  for (const dir of fs.readdirSync(path.join(__dirname, "fixtures"))) {
+    t(dir, path.resolve(path.join(__dirname, "fixtures", dir)))
+  }
+});
