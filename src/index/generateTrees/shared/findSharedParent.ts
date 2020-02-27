f8b050b55c7efa761643166f73ff11fcd634bcbc
Simon Jang <simon.jang.contact@gmail.com>
2020-02-27 23:43:53 +0100

    chore: PR feedback

--- src/index/generateTrees/shared/findSharedParent.ts
+++ src/index/generateTrees/shared/findSharedParent.ts
@@ -2,22 +2,20 @@ import path from "path";
 
 /** Find the common parent directory between all paths. */
 export const findSharedParent = (paths: string[]) => {
-  const [firstPath] = paths;
-  if (paths.length === 1) return path.dirname(firstPath);
+  if (paths.length === 1) return path.dirname(paths[0]);
 
-  const [shortest, secondShortest] = paths.sort((a, b) => a.length - b.length);
+  const [shortest, secondShortest] =
+    paths.length > 2 ? paths.sort((a, b) => a.length - b.length) : paths;
 
+  const secondShortestParts = secondShortest.split("/");
   const parentPaths: string[] = [];
-  const [shortestParts, secondShortestParts] = [
-    shortest,
-    secondShortest,
-  ].map(x => x.split("/"));
 
-  shortestParts.forEach((part, idx) => {
-    if (part === secondShortestParts[idx]) {
+  let index = 0;
+  for (const part of shortest.split("/")) {
+    if (part === secondShortestParts[index++]) {
       parentPaths.push(part);
     }
-  });
+  }
 
   return parentPaths.join("/");
 };
