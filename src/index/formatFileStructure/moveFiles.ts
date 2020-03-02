6dc2c0fbef032b21a1f116847c1104bfe7c7cbcb
Wayne Van Son <waynevspersonal@gmail.com>
2020-03-03 01:23:20 +1100

    renames checkLsFiles to isFileGitTracked as pure function.
    moves isFolderGitTracked to outside of loop

--- src/index/formatFileStructure/moveFiles.ts
+++ src/index/formatFileStructure/moveFiles.ts
@@ -2,13 +2,21 @@ import fs from "fs-extra";
 import path from "path";
 import Git from "simple-git/promise";
 
+async function isFileGitTracked(git: Git.SimpleGit, location: string) {
+  return git
+    .silent(true)
+    .raw(["ls-files", "--error-unmatch", location])
+    .then(() => true)
+    .catch(() => false);
+}
+
 /** Moves each file in the tree from old path to new path. */
 export async function moveFiles(
   tree: Record<string, string>,
   parentFolder: string
 ) {
   const git = Git(parentFolder);
-
+  const isFolderGitTracked = await git.checkIsRepo();
   for (const [oldPath, newPath] of Object.entries(tree)) {
     // skip globals
     if (oldPath.includes("..")) continue;
@@ -16,27 +24,19 @@ export async function moveFiles(
     const oldAbsolutePath = path.resolve(parentFolder, oldPath);
     const newAbsolutePath = path.resolve(parentFolder, newPath);
 
-    const isSamePath = oldAbsolutePath === newAbsolutePath;
     if (oldAbsolutePath === newAbsolutePath) continue;
 
     // Create folder for files
     const newDirname = path.dirname(newAbsolutePath);
     fs.ensureDirSync(newDirname);
 
-    /** Reference: https://git-scm.com/docs/git-ls-files#Documentation/git-ls-files.txt---error-unmatch */
-    const checkLsFiles = () => {
-      return (
-        git
-          .silent(true)
-          .raw(["ls-files", "--error-unmatch", oldAbsolutePath])
-          .then(() => true)
-          .catch(() => false)
-      );
-    };
+    const shouldGitMv =
+      isFolderGitTracked && (await isFileGitTracked(git, oldAbsolutePath));
 
-    const shouldGitMv = (await git.checkIsRepo()) && (await checkLsFiles());
-
-    if (shouldGitMv) await git.mv(oldAbsolutePath, newAbsolutePath);
-    else fs.renameSync(oldAbsolutePath, newAbsolutePath);
+    if (shouldGitMv) {
+      await git.mv(oldAbsolutePath, newAbsolutePath);
+    } else {
+      fs.renameSync(oldAbsolutePath, newAbsolutePath);
+    }
   }
 }
