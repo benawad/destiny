3b05b2a6b371bd53276bc316393dfd6883c001aa
AnatoleLucet <lucet.anatole@gmail.com>
2020-04-28 18:40:10 +0200

    fix: #111

--- src/index/shared/findImports.ts
+++ src/index/shared/findImports.ts
@@ -2,12 +2,22 @@ import fs from "fs";
 
 /** Find all imports for file path. */
 export function findImports(filePath: string) {
-  const reImport = /(?:(?:import|from)\s+|(?:import|require)\s*\()['"]((?:\.{1,2})(?:\/.+)?)['"]/gm;
+  // match es5 & es6 imports
+  const _reImport = `(?:(?:import|from)\\s+|(?:import|require)\\s*\\()['"]((?:\\.{1,2})(?:\\/.+)?)['"]`;
+  const reImport = new RegExp(_reImport, "gm");
+  // match one & multi line(s) comments
   const reComment = /\/\*[\s\S]*?\*\/|\/\/.*/gm;
+  // match string which contain an import https://github.com/benawad/destiny/issues/111
+  const reOneLineString = new RegExp(`["'].*(${_reImport}).*["']`, "g");
+  // match multi lines string which contain an import https://github.com/benawad/destiny/issues/111
+  const reMultiLinesString = new RegExp(`\`[^]*(${_reImport})[^]*\``, "gm");
+
   const importPaths: string[] = [];
   const fileContent = fs
     .readFileSync(filePath, { encoding: "utf8" })
-    .replace(reComment, "");
+    .replace(reComment, "")
+    .replace(reOneLineString, "")
+    .replace(reMultiLinesString, "");
 
   let matches;
   while ((matches = reImport.exec(fileContent)) !== null) {
